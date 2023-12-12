5cb30f1b16658ce69f24fc2e898aebe158562f48
peni414 <p.nissen.89@googlemail.com>
2023-12-12 21:13:46 +0100

    have a look to the payload

--- Classes/Domain/Finishers/SmtpFinisher.php
+++ Classes/Domain/Finishers/SmtpFinisher.php
@@ -1,8 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 namespace PeerNissen\SmtpFinisher\Domain\Finishers;
 
+use DateTime;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -85,7 +87,7 @@ final class SmtpFinisher extends AbstractFinisher
 
         $mailer->setFrom($this->options["senderAddress"]);
         foreach ($this->parseOption('notifying') as $notifyingEmail => $notifyingName) {
-            if($notifyingEmail != null && $notifyingName != null){
+            if ($notifyingEmail != null && $notifyingName != null) {
                 $mailer->addAddress($this->parseText($notifyingEmail), $this->parseText($notifyingName));
             } else if ($notifyingEmail != null && $notifyingName == null) {
                 $mailer->addAddress($this->parseText($notifyingEmail));
@@ -106,7 +108,7 @@ final class SmtpFinisher extends AbstractFinisher
      */
     private function confirmInput(): void
     {
-		$mailer = $this->phpMailerLib = new PHPMailer();
+        $mailer = $this->phpMailerLib = new PHPMailer();
 
         $mailer->isSMTP();
         $mailer->SMTPAuth = true;
@@ -125,7 +127,7 @@ final class SmtpFinisher extends AbstractFinisher
 
         $mailer->isHTML();
         $mailer->Subject = $this->parseText($this->options["subject"]);
-        $mailer->Body = $this->parseHtmlMarkup( (int)$this->parseOption('htmlBody'));
+        $mailer->Body = $this->parseHtmlMarkup((int)$this->parseOption('htmlBody'));
         $mailer->AltBody = $this->parseText($this->options["altBody"]);
 
         $mailer->Send();
@@ -133,13 +135,31 @@ final class SmtpFinisher extends AbstractFinisher
 
     private function getNotificationMessage(): string
     {
-        $result = "Jemand hat eine Anfrage gestellt:<br /><table>";
         $formValues = $this->finisherContext->getFormValues();
-        $keys = array_keys($formValues);
+        return json_encode($formValues);
+        // return "Jemand hat eine Anfrage gestellt:<br />" . $this->arrayToHtml($formValues);
+    }
+
+    private function arrayToHtml($array): string
+    {
+        $result = "<table>";
+        $keys = array_keys($array);
         foreach ($keys as $key) {
-            $result = $result . "<tr><td>" . $key . "</td><td>" . $formValues[$key] . "</td></tr>";
+            $result = $result . "<tr><td>" . $key . "</td><td>" . $this->arrayValueToHtml($array[$key]) . "</td></tr>";
         }
-        return $result . "</table>";
+        $result = $result . "</table>";
+        return $result;
+    }
+
+    private function arrayValueToHtml($arrayValue): string
+    {
+        if (is_null($arrayValue)) {
+            return "";
+        }
+        if (!is_array($arrayValue)) {
+            return $this->strToValue($arrayValue);
+        }
+        return $this->arrayToHtml($arrayValue);
     }
 
     private function parseText(string $s): string
@@ -148,9 +168,32 @@ final class SmtpFinisher extends AbstractFinisher
         $formValues = $this->finisherContext->getFormValues();
         $keys = array_keys($formValues);
         foreach ($keys as $key) {
-            $result = str_replace("{" . $key . "}", $formValues[$key], $result);
+            $result = str_replace("{" . $key . "}", $this->strToValue($this->getValueFromXdimensional($formValues, $key)), $result);
         }
         return $result;
+    }
+
+    private function strToValue($val)
+    {
+        if ($val instanceof DateTime) {
+            return $val->format('Y-m-d H:i:s');
+        }
+        return strval($val);
+    }
+
+    private function getValueFromXdimensional($array, $key)
+    {
+        foreach ($array as $k => $value) {
+            if ($key === $k) {
+                return $value;
+            } else if (is_array($value)) {
+                $result = $this->getValueFromXdimensional($value, $key);
+                if (!is_null($result)) {
+                    return $result;
+                }
+            }
+        }
+        return null;
     }
 
     private function parseHtmlMarkup(int $contentElementUid): string
@@ -181,7 +224,7 @@ final class SmtpFinisher extends AbstractFinisher
         $keys = array_keys($formValues);
 
         foreach ($keys as $key) {
-            $content = str_replace("{" . $key . "}", $formValues[$key], $content);
+            $content = str_replace("{" . $key . "}", $this->strToValue($this->getValueFromXdimensional($formValues, $key)), $content);
         }
 
         return $content;
